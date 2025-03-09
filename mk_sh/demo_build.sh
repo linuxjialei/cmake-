9ab5481155677976193e0ccb5c93c02172d4b6ae
jl <874727235@qq.com>
2025-03-09 11:53:10 +0800

    删除没用的语句

--- mk_sh/demo_build.sh
+++ mk_sh/demo_build.sh
@@ -27,18 +27,6 @@ if [ $? -eq 0 ]; then
 	if [ ! -d log ];then
 		mkdir log
 	fi
-	#拷贝相关库、代码调试工具、配置文件
-	cp ../tool/process_cfg.txt ./cfg/
-
-	#打包
-	tar zcf demo_update_soft.tar.gz ./bin ./lib ./cfg ./log 
-	mv demo_update_soft.tar.gz demo_app.img
-	md5sum demo_app.img>demo_app.img.md5
-	tar zcf demo_app.tar.gz  demo_app.img*
-	mv demo_app.tar.gz demo_app.img
-	mv demo_app.img ../mk_sh/
-	echo "Packaging successful, demo_ app.img location(current directory):/mk_sh/"
-	#解包tar -xzvf  demo_app.img 获取demo_app.img.md5 demo_app.img 计算MD5值与demo_app.img.md5文件内值相等后继续解压tar -xzvf  demo_app.img
 else
     echo "Compilation failed, please check the code！"
 fi
